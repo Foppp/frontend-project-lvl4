b8f4ca37a1618065c70031733c3bac2fd0d6cb06
Yura <tt@list.ru>
2021-07-24 22:27:42 +0300

    rollbar removed

--- src/index.js
+++ src/index.js
@@ -7,23 +7,15 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { useDispatch } from 'react-redux';
-import { Provider as ProviderRoll, ErrorBoundary } from '@rollbar/react'
 import store from './redux/store.js'
 import App from './App.jsx';
-
-const rollbarConfig = {
-  accessToken: 'POST_CLIENT_ITEM_ACCESS_TOKEN',
-  environment: 'production',
-};
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 render(
   <Provider store={store}>
-    <ProviderRoll config={rollbarConfig}>
       <App />
-      </ProviderRoll>
   </Provider>,
   document.getElementById('chat'),
 );
