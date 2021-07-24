1d754358179f23a68c43f1d95bb9270525a729b9
Yura <tt@list.ru>
2021-07-24 21:41:20 +0300

    rollbar added

--- src/index.js
+++ src/index.js
@@ -7,15 +7,23 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { useDispatch } from 'react-redux';
+import { Provider as ProviderRoll, ErrorBoundary } from '@rollbar/react'
 import store from './redux/store.js'
 import App from './App.jsx';
+
+const rollbarConfig = {
+  accessToken: "b2c6dd102a49436081b2b5f0003ad1e1",
+  environment: 'production',
+};
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 render(
   <Provider store={store}>
-    <App />
+    <ProviderRoll config={rollbarConfig}>
+      <App />
+      </ProviderRoll>
   </Provider>,
   document.getElementById('chat'),
 );
