b9c4efdc584bc1b7436e5c84c2a2b7778ced4f03
Yura <tt@list.ru>
2021-07-30 00:09:34 +0300

    new channel role button

--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -30,7 +30,7 @@ const Channels = () => {
     <div className="col-4 col-md-2 border-end pt-5 px-0 bg-light">
       <div className="d-flex justify-content-between mb-2 ps-4 pe-2">
         <span>{t('channels.title')}</span>
-        <button type="button" className="p-0 text-primary btn btn-group-vertical" onClick={handleOpenModal('addChannel')}>
+        <button type="button" role="button" className="p-0 text-primary btn btn-group-vertical" onClick={handleOpenModal('addChannel')}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"></path><path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"></path></svg>
           <span className="visually-hidden">+</span>
         </button>
@@ -58,6 +58,7 @@ const Channels = () => {
               <div role="group" className="d-flex dropdown btn-group">
                 <button
                   type="button"
+                  role="button"
                   className={classUnremovable}
                   onClick={handleSetCurrentChannel(id)}
                 >
@@ -68,6 +69,7 @@ const Channels = () => {
                   <>
                     <button
                       type="button"
+                      role="button"
                       className={classRemovable}
                       data-bs-toggle="dropdown"
                       aria-expanded="false"
