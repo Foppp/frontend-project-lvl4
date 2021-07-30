62553b31568d71e5bd493c73b6bc013e5856d7e5
Yura <tt@list.ru>
2021-07-30 19:07:23 +0300

    added autoscroll on new message

--- src/components/MessagesBody.jsx
+++ src/components/MessagesBody.jsx
@@ -1,14 +1,18 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 const MessagesBody = () => {
-  const divRef = useRef(null);
+  const messageBoxRef = useRef(null);
   const messages = useSelector((state) => state.messagesInfoReducer.messages);
   const currentChannelId = useSelector((state) => state.channelsInfoReducer.currentChannelId);
   const currentChannelMessages = messages.filter(({ channelId }) => channelId === currentChannelId);
 
+  useEffect(() => {
+    const messageBox = messageBoxRef.current;
+    messageBox.scrollTop = messageBox.scrollHeight;
+  });
   return (
-    <div id="messages-box" className="chat-messages overflow-auto px-5 ">
+    <div id="messages-box" ref={messageBoxRef} className="chat-messages overflow-auto px-5 ">
       {currentChannelMessages.map(({ body, id, username }) => (
         <div key={id} className="text-break mb-2">
           <b>{username}</b>
@@ -16,7 +20,6 @@ const MessagesBody = () => {
           {body}
         </div>
       ))}
-      <div ref={divRef} />
     </div>
   );
 };
