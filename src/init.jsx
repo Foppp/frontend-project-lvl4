d5e157e0236fd5736d94ed9a284b0f7de24f4651
Yura <tt@list.ru>
2021-07-30 19:29:12 +0300

    init locale

--- src/init.jsx
+++ src/init.jsx
@@ -7,41 +7,20 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
 import io from 'socket.io-client';
-import i18next from 'i18next';
-import { initReactI18next, I18nextProvider } from 'react-i18next';
-import * as Yup from 'yup';
+// import i18next from 'i18next';
+import { I18nextProvider } from 'react-i18next';
+// import * as Yup from 'yup';
 import App from './App.jsx';
 import store from './redux/store.js';
-import resources from './locales/index.js';
+import initLoclale from './utils/i18n.js';
+// import resources from './locales/index.js';
 
 const init = (socket) => {
   if (process.env.NODE_ENV !== 'production') {
     localStorage.debug = 'chat:*';
   }
-  const i18n = i18next.createInstance();
-  i18n
-    .use(initReactI18next)
-    .init({
-      resources,
-      lng: 'ru',
-      debug: false,
-      fallbackLng: 'en',
-      interpolation: {
-        escapeValue: false,
-      },
-    },
-    Yup.setLocale({
-      mixed: {
-        default: i18n.t('errors.unknown'),
-        required: i18n.t('errors.required'),
-        oneOf: () => i18n.t('errors.passMatch'),
-        notOneOf: () => i18n.t('errors.channelExist'),
-      },
-      string: {
-        min: ({ path }) => (path === 'password' ? i18n.t('errors.minCharactersPass') : i18n.t('errors.minCharacters')),
-        max: () => i18n.t('errors.maxCharacters'),
-      },
-    }));
+  const i18n = initLoclale();
+
   const vdom = (
     <Provider store={store}>
       <I18nextProvider i18n={i18n}>
@@ -58,4 +37,5 @@ const init = (socket) => {
 
 const userSocket = io();
 init(userSocket);
+
 export default init;
