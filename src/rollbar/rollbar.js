05100789d9a33aeddd625627b1d1e384529fe2be
Yura <tt@list.ru>
2021-07-30 21:25:56 +0300

    Added rollbarProvider and ErrorBoundary

--- src/rollbar/rollbar.js
+++ src/rollbar/rollbar.js
@@ -1,5 +1,5 @@
 const rollbarConfig = {
-  accessToken: process.env.ACCESS_TOKEN_ROLBAR,
+  accessToken: 'b2c6dd102a49436081b2b5f0003ad1e1',
   captureUncaught: true,
   captureUnhandledRejections: true,
   payload: {
