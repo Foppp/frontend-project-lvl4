24a342b452e125fb43f6e384f1fd04ad2924a2f4
Yura <tt@list.ru>
2021-07-30 19:38:06 +0300

    deleted logout component

--- src/init.jsx
+++ src/init.jsx
@@ -5,15 +5,11 @@ import 'regenerator-runtime/runtime.js';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-
 import io from 'socket.io-client';
-// import i18next from 'i18next';
 import { I18nextProvider } from 'react-i18next';
-// import * as Yup from 'yup';
 import App from './App.jsx';
 import store from './redux/store.js';
 import initLoclale from './utils/i18n.js';
-// import resources from './locales/index.js';
 
 const init = (socket) => {
   if (process.env.NODE_ENV !== 'production') {
